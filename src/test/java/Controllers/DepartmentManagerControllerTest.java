b5c2ad57ed9129eda40d8b8ec2ed1225c83caa8d
abderrahmane <abderrahamane7@gmail.com>
2022-10-31 14:44:34 +0100

    testing the login method for the departmentManager

--- src/test/java/Controllers/DepartmentManagerControllerTest.java
+++ src/test/java/Controllers/DepartmentManagerControllerTest.java
@@ -6,12 +6,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepartmentManagerControllerTest {
-
+    /*
+     * Save method test
+     * */
     @Test
     public void saveDepartmentManagerTest(){
         Departmentmanager departmentmanager = DepartmentManagerController.save("dpt1.manager@marjane.com", "dptmanager1", 1, 1);
 
         assertNotNull(departmentmanager);
         assertNotNull(departmentmanager.getId());
+    }
+
+    /*
+     * Login method tests
+     * */
+    @Test
+    public void departmentManagerLoginTestWithValidEmailAndPassword(){
+        Departmentmanager departmentmanager = DepartmentManagerController.login("dpt1.manager@marjane.com", "dptmanager1");
+
+        assertNotNull(departmentmanager);
+        assertNotNull(departmentmanager.getId());
+    }
+    @Test
+    public void departmentManagerLoginTestWithInvalidEmailAndPassword(){
+        Departmentmanager departmentmanager = DepartmentManagerController.login("dpt1.manager@marjane.com", "testing");
+
+        assertNull(departmentmanager);
     }
 }
