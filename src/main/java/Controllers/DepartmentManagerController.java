0ec7eeb02e39085051671ce98923ae7147bc67f8
abderrahmane <abderrahamane7@gmail.com>
2022-10-31 14:40:16 +0100

    added login and save method to departmentManager Controller

--- src/main/java/Controllers/DepartmentManagerController.java
+++ src/main/java/Controllers/DepartmentManagerController.java
@@ -1,4 +1,36 @@
 package Controllers;
 
+import Dao.DepartmentManagerDao;
+import Security.BCrypt;
+import entities.Departmentmanager;
+import jakarta.persistence.Query;
 public class DepartmentManagerController {
+    public static Departmentmanager login(String email, String password){
+        try {
+            Query query = JPA.Utils.Helper.entityManager().createNamedQuery("DepartmentManager.getByEmail").setParameter(1, email);
+            Departmentmanager departmentmanager = (Departmentmanager) query.getResultList().get(0);
+
+            if (!BCrypt.checkpw(password, departmentmanager.getPassword()))
+                return null;
+            return departmentmanager;
+        }catch (Exception e){
+            e.getStackTrace();
+            return null;
+        }
+    }
+
+    public static Departmentmanager save(String email, String password, Integer departmentId, Integer storeId){
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
+
+        Departmentmanager departmentmanager = new Departmentmanager();
+
+        departmentmanager.setEmail(email);
+        departmentmanager.setPassword(hashedPassword);
+        departmentmanager.setDepartmentid(departmentId);
+        departmentmanager.setStoreid(storeId);
+
+        DepartmentManagerDao departmentManagerDao = new DepartmentManagerDao();
+        departmentManagerDao.save(departmentmanager);
+        return departmentmanager;
+    }
 }
