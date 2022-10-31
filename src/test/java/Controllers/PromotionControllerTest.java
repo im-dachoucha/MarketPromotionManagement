fded8caac6297ecabf425b11a9ffc16ac5aecfeb
abderrahmane <abderrahamane7@gmail.com>
2022-10-31 16:31:04 +0100

    testing save promotion method

---
+++ src/test/java/Controllers/PromotionControllerTest.java
@@ -0,0 +1,40 @@
+package Controllers;
+
+import entities.Promotion;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PromotionControllerTest {
+
+    /*
+     * Save method tests
+     * */
+    @Test
+    public void savePromotionTestWithInvalidStartDate1(){
+        Promotion promotion = PromotionController.save("desc", "2000-12-12", "2022-12-12", BigDecimal.valueOf(.3d), 1, 1);
+
+        assertNull(promotion);
+    }
+    @Test
+    public void savePromotionTestWithInvalidStartDate2(){
+        Promotion promotion = PromotionController.save("desc", "2022-12-21", "2022-12-13", BigDecimal.valueOf(.3d), 1, 1);
+
+        assertNull(promotion);
+    }
+    @Test
+    public void savePromotionTestWithInvalidDiscount(){
+        Promotion promotion = PromotionController.save("desc", "2022-12-01", "2022-12-15", BigDecimal.valueOf(.6d), 1, 1);
+
+        assertNull(promotion);
+    }
+    @Test
+    public void savePromotionWithValidData(){
+        Promotion promotion = PromotionController.save("desc", "2022-12-01", "2022-12-15", BigDecimal.valueOf(.3d), 1, 1);
+
+        assertNotNull(promotion);
+        assertNotNull(promotion.getId());
+    }
+}
