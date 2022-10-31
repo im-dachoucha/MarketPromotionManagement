dc51b52da5e82cbda8c036389bb333f9512fd05e
abderrahmane <abderrahamane7@gmail.com>
2022-10-31 13:49:50 +0100

    added tests for the admin controller

---
+++ src/test/java/Controllers/AdminControllerTest.java
@@ -0,0 +1,27 @@
+package Controllers;
+
+import entities.Admin;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+
+    @Test
+    public void adminLoginTestWithValidEmailAndPassword(){
+        Admin admin = AdminController.login("test@email.com", "test");
+        assertNotNull(admin);
+    }
+
+    @Test
+    public void adminLoginTestWithValidEmailAndInvalidPassword(){
+        Admin admin = AdminController.login("test@email.com", "123");
+        assertNull(admin);
+    }
+
+    @Test
+    public void adminLoginTestWithInvalidEmailAndPassword(){
+        Admin admin = AdminController.login("mail@email.com", "test");
+        assertNull(admin);
+    }
+}
