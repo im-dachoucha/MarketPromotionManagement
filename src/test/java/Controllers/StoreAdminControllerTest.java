5ed01f6e87cd545f9447c1c0b5881c5f9408f046
abderrahmane <abderrahamane7@gmail.com>
2022-10-31 14:35:00 +0100

    testing save method for storeAdmin Controller

--- src/test/java/Controllers/StoreAdminControllerTest.java
+++ src/test/java/Controllers/StoreAdminControllerTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StoreAdminControllerTest {
 
+    /*
+    * Login method tests
+    * */
     @Test
     public void storeAdminLoginTestWithValidEmailAndPassword(){
         Storeadmin storeadmin = StoreAdminController.login("email@email.com", "test");
@@ -17,5 +20,16 @@ class StoreAdminControllerTest {
     public void storeAdminLoginTestWithInvalidEmailAndPassword(){
         Storeadmin storeadmin = StoreAdminController.login("test@email.com", "123");
         assertNull(storeadmin);
+    }
+
+    /*
+     * Save method tests
+     * */
+    @Test
+    public void saveStoreAdminTest(){
+        Storeadmin storeadmin = StoreAdminController.save("store.admin1@marjane.com", "storeadmin1", 1);
+
+        assertNotNull(storeadmin);
+        assertNotNull(storeadmin.getId());
     }
 }
