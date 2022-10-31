d010de9e68a14d539f5374dae3c71171b81b0dc4
abderrahmane <abderrahamane7@gmail.com>
2022-10-31 14:34:34 +0100

    added save method for storeAdmin Controller

--- src/main/java/Controllers/StoreAdminController.java
+++ src/main/java/Controllers/StoreAdminController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Dao.StoreAdminDao;
 import Security.BCrypt;
 import entities.Storeadmin;
 import jakarta.persistence.Query;
@@ -18,5 +19,18 @@ public class StoreAdminController {
             e.getStackTrace();
             return null;
         }
+    }
+
+    public static Storeadmin save(String email, String password, Integer storeId){
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
+
+        Storeadmin storeadmin = new Storeadmin();
+        storeadmin.setEmail(email);
+        storeadmin.setPassword(hashedPassword);
+        storeadmin.setStoreid(storeId);
+
+        StoreAdminDao storeAdminDao = new StoreAdminDao();
+        storeAdminDao.save(storeadmin);
+        return storeadmin;
     }
 }
